b7d4e7117c07facc0300629e609cc3907a20870f
ramonestii <ramonestii@hotmail.com>
2014-09-26 12:36:10 -0300

    Criação da Classe Relogio

---
+++ Program_Robocop/Robocop.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Robocop.cpp
+ * Author: Asus
+ * 
+ * Created on 26 de Setembro de 2014, 12:35
+ */
+
+#include "Robocop.h"
+
+Robocop::Robocop() {
+}
+
+Robocop::Robocop(const Robocop& orig) {
+}
+
+Robocop::~Robocop() {
+}
+
---
+++ Program_Robocop/Robocop.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Robocop.h
+ * Author: Asus
+ *
+ * Created on 26 de Setembro de 2014, 12:35
+ */
+
+#ifndef ROBOCOP_H
+#define	ROBOCOP_H
+
+class Robocop {
+public:
+    Robocop();
+    Robocop(const Robocop& orig);
+    virtual ~Robocop();
+private:
+
+};
+
+#endif	/* ROBOCOP_H */
+
